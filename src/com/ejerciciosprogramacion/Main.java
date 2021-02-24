7465073c2c43cb869302415428c110b95a42cd8a
Jxrge <72072830+Jxrge@users.noreply.github.com>
2021-02-24 12:31:23 +0100

    Update Main.java

--- src/com/ejerciciosprogramacion/Main.java
+++ src/com/ejerciciosprogramacion/Main.java
@@ -1,6 +1,6 @@
 package com.ejerciciosprogramacion;
 
-public class Main {
+public class Boletin14 {
 
     public static void main(String[] args) {
         ConversorTemperaturas temperatura1 = new ConversorTemperaturas();
